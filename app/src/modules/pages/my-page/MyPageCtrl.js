4dd73dc2b75795832e533298e7482f26a6e16b24
xiili@yahoo.com <xiili@yahoo.com>
2015-09-29 13:52:53 -0500

    add new route

--- app/src/modules/pages/my-page/MyPageCtrl.js
+++ app/src/modules/pages/my-page/MyPageCtrl.js
@@ -5,5 +5,7 @@
 
 angular.module('myApp').controller('MyPageCtrl', ['$scope',
 function($scope) {
-	
+$scope.items = [ 
+      {title:'title 1'},{title:'title 2'},{title: 'title 3'}, {title:'title 4'}
+ ];	
 }]);
